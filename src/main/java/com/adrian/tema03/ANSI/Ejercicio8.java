eda03d0b8a1ae09ae8e3914ab59dde55dcc7c455
Adrian Hustea <adriannicolash3@gmail.com>
2024-10-16 15:53:29 +0200

    hola

--- src/main/java/com/adrian/tema03/ANSI/Ejercicio8.java
+++ src/main/java/com/adrian/tema03/ANSI/Ejercicio8.java
@@ -2,16 +2,23 @@ package com.adrian.tema03.ANSI;
 
 public class Ejercicio8 {
     public static void main(String[] args) {
-        String cadena = "";
         int color = 29;
         int fondo = 39;
-        for (int i = 1; i <= 9; i++) {
-            cadena = cadena + i;
-            color = color +1 ;
-            if (color == 38){
-                color =30;
+        for (int negrita = 0; negrita <= 1; negrita++) {
+            for (int j = 1; j <= 8; j++) {
+                color = color +1;
+                if (color == 38){
+                    color =30;
+                }
+                for (int i = 1; i <= 8; i++) {
+                    fondo = fondo +1 ;
+                    if (fondo == 48){
+                        fondo =40;
+                    }
+                    System.out.printf("\u001B["+negrita+";"+color+";"+fondo+"m "+negrita+";"+color+";"+fondo+" \u001B[0m" ,cadena);
+                }
+                System.out.println("");
             }
-            System.out.printf("\u001B[1;" + color + ";" +fondo+ "m %9s \u001B[0m \n" ,cadena);
         }
     }
 }
